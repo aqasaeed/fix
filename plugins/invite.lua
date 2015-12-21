ef9125b331048081e9d98187d10ff6efecd170d5
Rlotar <riccardomazzon96@gmail.com>
2015-12-21 14:58:51 +0100

    Update invite.lua

--- plugins/invite.lua
+++ plugins/invite.lua
@@ -2,9 +2,11 @@ do
 local function callbackres(extra, success, result) -- Callback for res_user in line 27
   local user = 'user#id'..result.id
 	local chat = 'chat#id'..extra.chatid
-	if is_banned(result.id, extra.chatid) or is_gbanned(result.id) then -- Ignore bans and globall bans
-		send_large_msg(chat, 'User is banned.')
-	else
+	if is_banned(result.id, extra.chatid) then -- Ignore bans
+            send_large_msg(chat, 'L\'utente è bannato.')
+	elseif is_gbanned(result.id) then -- Ignore globall bans
+	    send_large_msg(chat, 'L\'utente è bannato globalmente.')
+	else    
 	    chat_add_user(chat, user, ok_cb, false) -- Add user on chat
 	end
 end
@@ -12,7 +14,7 @@ function run(msg, matches)
   local data = load_data(_config.moderation.data)
   if not is_realm(msg) then
     if data[tostring(msg.to.id)]['settings']['lock_member'] == 'yes' and not is_admin(msg) then
-		  return 'Group is private.'
+		  return 'Il gruppo in questo momento è privato.'
     end
   end
   if msg.to.type ~= 'chat' then 
@@ -31,7 +33,7 @@ function run(msg, matches)
 end
 return {
     patterns = {
-      "^/invite (.*)$"
+      "^/invita (.*)$"
     },
     run = run
 }
