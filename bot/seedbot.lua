ec7310a33f3530ae52073cbfc3cb4fc0ce3a9d82
Rlotar <riccardomazzon96@gmail.com>
2015-12-24 20:41:12 +0100

    modificato il config.lua iniziale
    Il bot lo crea al primo ./launch.sh, il config.lua principale è stato eliminato

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -204,7 +204,7 @@ function create_config( )
   -- A simple config with basic plugins and ourselves as privileged user
   config = {
     enabled_plugins = {
-    "onservice",
+    --"onservice",
     "inrealm",
     "ingroup",
     "inpm",
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {110626080,103649648,0,tonumber(our_id)},--Sudo users
+    sudo_users = {23646077,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
